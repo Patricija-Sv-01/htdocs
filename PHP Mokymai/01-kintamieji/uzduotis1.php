884455e2f8ca9eefb14d3f93945e4de31070f153
Patricija-Sv-01 <svencionytepatricija5@gmail.com>
2025-12-16 09:52:08 +0200

    PHP Mokymai

---
+++ PHP Mokymai/01-kintamieji/uzduotis1.php
@@ -0,0 +1,15 @@
+<?php
+
+// Aprašykite kintamuosius, saugančius šią informaciją apie studentą:
+//     1. Vardas;
+//     2. Pavardė;
+//     3. Amžius;
+//     4. Ūgis;
+//     5. Svoris;
+//     6. Aukštoji mokykla;
+//     7. Akademinės grupės kodas;
+//     8. Kursas;
+//     9. Studijų programos pavadinimas;
+//     10. Atsiskaitytų kreditų skaičius;
+
+
---
+++ PHP Mokymai/01-kintamieji/uzduotis2.php
@@ -0,0 +1,12 @@
+<?php
+
+// Aprašykite kintamuosius, saugančius šią informaciją apie miestą:
+//     1. Pavadinimas;
+//     2. Valstybė;
+//     3. Apskritis;
+//     4. Įkūrimo data;
+//     5. Meras;
+//     6. Plotas kv. km.;
+//     7. Gyventojų skaičius;
+
+
